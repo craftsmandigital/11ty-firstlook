a48cbe96a569300afa5ad45b96ae12a1c5e1ea23
craftsmandigital <hackjack@tutanota.com>
2022-03-22 18:16:55 +0100

    Blogpost reshources in folder. Same as HUGO functionality. Also pastrough copy of images

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
+  // Copy any .jpg file to `_site`, via Glob pattern
+  // Keeps the same directory structure.
   eleventyConfig.addPassthroughCopy("./src/tailwind.css");
+  eleventyConfig.addPassthroughCopy("./src/**/*.jpg");
   return {
     dir: {
       input: "src"
